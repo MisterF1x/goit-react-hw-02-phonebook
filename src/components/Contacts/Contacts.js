418e9eea7e60e3b30bc9eb1adeab19d4dc5cba2f
X Mister <someinkognito@gmail.com>
2023-04-22 02:54:41 +0300

    phonebook app

---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import { IconSize } from 'components/constant';
+import { ContactItem, ContactParagraph, TrashBtn } from './Contacts.styled';
+import { FaTrashAlt } from 'react-icons/fa';
+
+export const Contacts = ({ contacts, onDeleteContact }) => {
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => {
+        return (
+          <ContactItem key={id}>
+            <ContactParagraph>{name}</ContactParagraph>
+            <ContactParagraph>{number}</ContactParagraph>
+            <TrashBtn
+              type="button"
+              aria-label="delete"
+              onClick={() => onDeleteContact(id)}
+            >
+              <FaTrashAlt size={IconSize.sm} />
+            </TrashBtn>
+          </ContactItem>
+        );
+      })}
+    </ul>
+  );
+};
+
+Contacts.propTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+};
